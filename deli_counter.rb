62ec95999e86a84916326107e5ef872e697cf080
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-25 03:18:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
 def take_a_number(katz_deli, name)
       katz_deli.push(name)
     	puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
-  end
+end
   
 
 # def take_a_number(katz_deli, name)
