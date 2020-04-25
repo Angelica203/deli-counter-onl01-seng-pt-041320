bc4b03a7daa2853ac7c921e503322e919186e815
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-25 02:39:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,45 @@
-# Write your code here.
+# katz_deli = []
+ 
+# def take_a_number(array, name)
+#   array.push(name)
+#   position = array.index(name)
+# end
+# 
+
+def line(katz_deli)
+  if katz_deli.size == 0
+     puts "The line is currently empty."
+  else
+    new_array = []
+    katz_deli.each_with_index {|name, i| new_array << "#{i + 1}. #{name}"}
+    	puts  "The line is currently: " << new_array.join(" ")
+  end
+end
+
+def take_a_number(katz_deli, name)
+   if katz_deli = (name)
+      puts "Welcome, #{name}. You are number 1 in line."
+     else  katz_deli = (name)
+      # else new_array =[name]
+        # katz_deli = (name)
+      end
+    end
+    
+#   # puts "there is nobody in line"
+# else new_array =[]
+#   # puts "Welcome, #{name}. You are number 1 in line."
+#   katz_deli = (name)
+ 
+   
+   
+   
+   
+   
+#   array.push(name)
+#   position = array.index(name)
+#   puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+#   return name, position
+# end
+# end
+
+
